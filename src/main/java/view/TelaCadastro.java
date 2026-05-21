e5ef14aff18cc749d2b22aee5da77ff5694fcded
Victor <victor.montes@montreal.com.br>
2026-05-21 17:25:38 -0300

    Inclusão de máscara para o CPF e eventos de clicked e released

--- src/main/java/view/TelaCadastro.java
+++ src/main/java/view/TelaCadastro.java
@@ -5,15 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.Font;
+import java.awt.event.*;
+import java.text.ParseException;
 import javax.swing.JPasswordField;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import javax.swing.JFormattedTextField;
+import javax.swing.SwingUtilities;
 
 public class TelaCadastro extends JFrame {
 
@@ -24,7 +29,7 @@ public class TelaCadastro extends JFrame {
 	private JTextField campoTelefone;
 	private JPasswordField campoSenha;
 	private JPasswordField campoConfirmarSenha;
-	private JTextField campoCpf;
+	private JFormattedTextField campoCpf;
 
 	/**
 	 * Launch the application.
@@ -54,88 +59,111 @@ public class TelaCadastro extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		this.setTitle("Tela Cadastro");
-		
+		this.setTitle("QLAB - Cadastro de Usuário");
+		setResizable(false);
+
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.setForeground(Color.WHITE);
 		btnCadastrar.setBackground(new Color(34, 139, 34));
 		btnCadastrar.setBounds(246, 491, 152, 50);
 		contentPane.add(btnCadastrar);
-		
+
 		campoNome = new JTextField();
 		campoNome.setFont(new Font("Tahoma", Font.BOLD, 11));
 		campoNome.setBounds(123, 178, 474, 33);
 		contentPane.add(campoNome);
 		campoNome.setColumns(10);
-		
+
 		campoEmail = new JTextField();
 		campoEmail.setFont(new Font("Tahoma", Font.BOLD, 11));
 		campoEmail.setBounds(123, 307, 474, 33);
 		contentPane.add(campoEmail);
 		campoEmail.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("Senha");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel.setBounds(55, 432, 46, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel labelNome = new JLabel("Nome");
 		labelNome.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		labelNome.setBounds(55, 185, 46, 14);
 		contentPane.add(labelNome);
-		
+
 		campoTelefone = new JTextField();
 		campoTelefone.setFont(new Font("Tahoma", Font.BOLD, 11));
 		campoTelefone.setBounds(123, 368, 474, 33);
 		contentPane.add(campoTelefone);
 		campoTelefone.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Confirmar Senha");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(308, 432, 141, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Email");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_2.setBounds(55, 314, 46, 14);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_2_1 = new JLabel("Telefone");
 		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_2_1.setBounds(55, 375, 73, 14);
 		contentPane.add(lblNewLabel_2_1);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setFont(new Font("Tahoma", Font.BOLD, 13));
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"    Administrador", "    Professor", "    Técnico", "    Guardião"}));
 		comboBox.setBounds(277, 116, 141, 28);
 		contentPane.add(comboBox);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Perfil");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_3.setBounds(234, 121, 46, 14);
 		contentPane.add(lblNewLabel_3);
-		
+
 		campoSenha = new JPasswordField();
 		campoSenha.setFont(new Font("Tahoma", Font.BOLD, 11));
 		campoSenha.setBounds(124, 425, 174, 33);
 		contentPane.add(campoSenha);
-		
+
 		campoConfirmarSenha = new JPasswordField();
 		campoConfirmarSenha.setFont(new Font("Tahoma", Font.BOLD, 11));
 		campoConfirmarSenha.setBounds(426, 423, 174, 33);
 		contentPane.add(campoConfirmarSenha);
-		
+
 		JLabel labelCpf = new JLabel("CPF");
 		labelCpf.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		labelCpf.setBounds(55, 250, 46, 14);
 		contentPane.add(labelCpf);
-		
-		campoCpf = new JTextField();
+
+		campoCpf = new JFormattedTextField();
 		campoCpf.setFont(new Font("Tahoma", Font.BOLD, 11));
 		campoCpf.setColumns(10);
 		campoCpf.setBounds(123, 243, 474, 33);
 		contentPane.add(campoCpf);
+		campoCpf.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				SwingUtilities.invokeLater(() -> campoCpf.setCaretPosition(0));
+			}
+		});
+
+		campoCpf.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				campoCpf.setCaretPosition(0);
+			}
+		});
+		
+		MaskFormatter mascaraCpf;
+		try {
+			mascaraCpf = new MaskFormatter("###.###.###-##");
+			mascaraCpf.setPlaceholderCharacter('_');
+			mascaraCpf.install(campoCpf);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		JLabel labelCadastro = new JLabel("Cadastro");
 		labelCadastro.setFont(new Font("Tahoma", Font.PLAIN, 50));
@@ -145,7 +173,6 @@ public class TelaCadastro extends JFrame {
 		JLabel labelIcone = new JLabel("Icone");
 		labelIcone.setBounds(71, 53, 46, 14);
 		contentPane.add(labelIcone);
-		
 		
 		}
 	}
