1c597b011a5fb1025fd41704148255a37100ded1
HeitorSantana <151783683+HeitorSantana@users.noreply.github.com>
2026-05-25 18:04:28 -0300

    Fazendo o metodo de CadastrarUsuarios no UsarioDao(insert)

---
+++ src/main/java/dao/UsuarioDao.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Usuario;
+import util.ConnectionFactory;
+
+public class UsuarioDao {
+	public void cadastrarUsuario(Usuario usuario) throws SQLException {
+		String sql = "insert into usuario (nome,email,cpf,telefone,senha,perfil) values(?,?,?,?,?,?)";
+		Connection conn = ConnectionFactory.getConnection();
+		PreparedStatement ps = conn.prepareStatement(sql);
+		
+		ps.setString(1, usuario.getNome());
+		ps.setString(2, usuario.getEmail());
+		ps.setString(3, usuario.getCpf());
+		ps.setString(4, usuario.getTelefone());
+		ps.setString(5,usuario.getSenha());
+		ps.setString(6, usuario.getPerfil().name());
+		
+		ps.executeUpdate();
+		
+		ConnectionFactory.closeConnection(conn);
+		ps.close();
+	}
+}
