770c2bb82af296c0d4b52dbc74647ec6300546bd
Márcio Luan <marcioluan18@hotmail.com>
2026-06-23 19:42:45 -0300

    feat: adicionar ddl de tabela horarios e reservas

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -27,6 +27,28 @@ INSERT INTO laboratorios (nome, status_laboratorio, qtd_computadores, descricao)
 ('Lab 05', 'LIVRE', 15, 'Sala do diretório acadêmico'),
 ('Lab 06', 'LIVRE', 15, 'Prática de IA'),
 ('Lab 07', 'LIVRE', 15, 'Informática 2');
-	descricao VARCHAR (80) NOT NULL
-	);
 
+
+CREATE TABLE IF NOT EXISTS horarios(
+	id_horario INT AUTO_INCREMENT PRIMARY KEY,
+	dia DATE NOT NULL,
+	horario ENUM (
+		'M1', 'M2', 'M3', 'M4', 'M5', 'M6',
+		'T1', 'T2', 'T3', 'T4', 'T5', 'T6',
+		'N1', 'N2', 'N3', 'N4', 'N5', 'N6') NOT NULL,
+	status_lab ENUM ('LIVRE', 'OCUPADO') DEFAULT 'LIVRE',
+	laboratorio_id INT NOT NULL,
+	FOREIGN KEY (laboratorio_id) REFERENCES laboratorios(id),
+	UNIQUE (dia, horario, laboratorio_id));
+	
+CREATE TABLE IF NOT EXISTS reservas(
+	id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+	
+	horario_id INT NOT NULL,
+	usuario_id INT NOT NULL,
+	
+	data_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	disciplina VARCHAR (80),
+	
+	FOREIGN KEY (horario_id) REFERENCES horarios(id_horario),
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id));
--- src/main/java/view/TelaDisponibilidadePorDia.java
+++ src/main/java/view/TelaDisponibilidadePorDia.java
@@ -198,9 +198,14 @@ public class TelaDisponibilidadePorDia extends JFrame {
 				String[] opcoesReserva = {"Reservar", "Cancelar", "Interditar"};
 				int linha = tabelaLaboratorios.rowAtPoint(e.getPoint());
 				int coluna = tabelaLaboratorios.columnAtPoint(e.getPoint());
-				var opcao = JOptionPane.showOptionDialog(null, "Deseja reservar?", "Reserva de horário", 2, 1, null, opcoesReserva, null);
+				String usuario = 
+				
+				var opcao = JOptionPane.showOptionDialog(null, "O que você deseja fazer?", "Reserva de horário", 2, 1, null, opcoesReserva, null);
 				
 				if (opcao == 0) {
+					String disciplina = JOptionPane.showInputDialog("Disciplina: ");
+					
+					
 					tabelaLaboratorios.setValueAt("OCUPADO", linha, coluna);
 				} else if (opcao == 1) {
 					tabelaLaboratorios.setValueAt("", linha, coluna);
